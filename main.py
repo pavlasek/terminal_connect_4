523cc00d0277790577f0969d22cb7dde419bb19d
pavlasek <149815098+pavlasek@users.noreply.github.com>
2025-03-09 22:36:25 +0100

    Add function to get keys.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import os
 import platform
 import random
+import sys
+import tty
+import termios
+import time
+import select
+
 
 class Connect4Game:
     game_grid = []
     cursor_position = 5
     cursor_symbol = "$"
+    first_player = "X"
+    second_player = "O"
 
     def __init__(self):
         self.game_grid = [
@@ -22,13 +30,54 @@ class Connect4Game:
         self.showGrid(self.cursor_symbol)
         while end_of_the_game == False:
             key = self.waitForInput()
-            self.move_cursor(key)
-
-            continue
+            #self.game_move(key)
+            winning_player = self.checkWinner()
+            if winning_player == None:
+                continue
+            else:
+                end_of_the_game = True
+                self.showFinalInfo(winning_player)
 
     def waitForInput(self):
-        pass
-        
+        key_pressed = False
+        while key_pressed == False:
+            key = self.get_key()
+            
+            if key == None:
+                continue
+
+            print(f"You pressed: {key}")
+            if key == 'q':
+                exit()
+
+
+
+    def get_key(self):
+        """Reads the key from the terminal."""
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            key_start = sys.stdin.read(1)
+
+            if key_start == '\x1b':  
+                key_end = key_start + sys.stdin.read(2)
+
+                if key_end == '\x1b[B':
+                    return 'DOWN'
+                elif key_end == '\x1b[C':  
+                    return 'RIGHT'
+                elif key_end == '\x1b[D':
+                    return 'LEFT'
+
+            else:
+                if key_start in {'q', 'a', 's', 'd'}:
+                    return key_start
+
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings) 
+
+
     def showStartingInfo(self):
         """Prints information at the start of the game. Prints instruction
         and how to play the game."""
@@ -39,7 +88,7 @@ class Connect4Game:
         print("How to play: ")
         print(" + Players takes turns to inserts stones into grid using.")
         print(" + Switch between columns using LEFT and RIGHT arrow and put the stone using the DOWN arrow.")
-        print(" + Blinking # symbol shows in which column the stone will be placed.")
+        print(" + # symbol shows in which column the stone will be placed.")
         print(" + Second player uses A, D and S keys.")
         print(" + Player who connects 4 in row, column or diagonally wins.")
         print("\n--------------------------------------------------------------------------------------------\n")
@@ -47,8 +96,18 @@ class Connect4Game:
     def checkWinner(self):
         pass
 
-    def showFinalInfo(self):
-        pass
+    def showFinalInfo(self, winning_player):
+        """Prints information at the end of the game."""
+        if winning_player == 0:
+            print("--------------------------------------------------------------------------------------------")
+            print("IT IS A DRAW!")
+        elif winning_player == 1:
+            print("--------------------------------------------------------------------------------------------")
+            print("PLAYER1 WINS!")
+        elif(winning_player == 2):
+            print("--------------------------------------------------------------------------------------------")
+            print("PLAYER2 WINS!")
+        
 
     def clearTerminal(self):
         """Clears terminal based on which operating system the game is running."""
